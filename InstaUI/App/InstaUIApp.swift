a1e13fa860c9726b873849fedcf623a5fa14cfd3
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-05-22 09:31:46 -0300

    folder structure

---
+++ InstaUI/App/InstaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaUIApp.swift
+//  InstaUI
+//
+//  Created by Welliton da Conceicao de Araujo on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
